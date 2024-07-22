44136f45f6d946722788b48500fc0764f37226dd
harshil0217 <harshil0217@gmail.com>
2024-07-22 13:00:11 -0400

    added api call to recruiting api

--- data_extract.py
+++ data_extract.py
@@ -1,5 +1,21 @@
-from __future__ import print_function
-import time
+
 import cfbd
 from cfbd.rest import ApiException
 from pprint import pprint
+
+#configure API key
+configuration = cfbd.Configuration()
+configuration.api_key['Authorization'] = 'V0gKQ2NsBKqVWwtw+rG4Xwb+RymmYZavnzylQCaDllBrinJFAeVFZCyHAxk0a8px'
+configuration.api_key_prefix['Authorization'] = 'Bearer'
+
+#create instance of recruiting API
+recruiting_api = cfbd.RecruitingApi(cfbd.ApiClient(configuration))
+year = 56
+
+try:
+    # Betting lines
+    api_response = recruiting_api.get_recruiting_teams(year=2023)
+    pprint(api_response)
+except ApiException as e:
+    print("did not work")
+
