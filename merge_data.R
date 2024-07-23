5d91e3502bc44c7ba74fcf5e0981eabe49617502
harshil0217 <harshil0217@gmail.com>
2024-07-23 14:26:57 -0400

    filtered to power 5

--- merge_data.R
+++ merge_data.R
@@ -1,16 +1,25 @@
 library(dplyr)
+library(tidyr)
 cfb_stats = read.csv("cfb23.csv")
-cfb_stats$Team = gsub("\\s*\\([^\\)]+\\)", "", cfb_stats$Team)
-cfb_stats$Team <- gsub("\\bSt\\.\\b", "State", cfb$Team)
 
 cfb_stats = cfb_stats %>%
-  mutate(Team = recode(Team, "App State" = "Appalachian State",
-                       ))
+  separate(Team, into = c("Team", "Conference"), sep = " \\(|\\)", extra = "drop", fill = "right") %>%
+  filter(Conference %in% c('Big 12', 'SEC', 'Pac-12', 'Big Ten', 'ACC'))
+
+
+cfb_stats$Team = gsub("\\bSt\\.", "State", cfb_stats$Team)
+
 
 recruiting = read.csv("recruiting_rankings.csv")
 
-cfb_stats_2 = merge(cfb_stats, recruiting, by = "Team")
+recruiting = recruiting %>%
+  mutate(Team = recode(Team, 'USC' = 'Southern California'))
 
+
+
+cfb_stats_2 = merge(cfb_stats, recruiting, by = "Team")
+diff = setdiff(cfb_stats$Team, cfb_stats_2$Team)
+print(diff)
 
 
 
