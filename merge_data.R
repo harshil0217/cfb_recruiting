ff563119235221241da8cffcbb2758062576a8b2
harshil0217 <harshil0217@gmail.com>
2024-07-23 14:23:33 -0400

    filtered to power five schools

--- merge_data.R
+++ merge_data.R
@@ -1,10 +1,17 @@
 library(dplyr)
 cfb_stats = read.csv("cfb23.csv")
 cfb_stats$Team = gsub("\\s*\\([^\\)]+\\)", "", cfb_stats$Team)
+cfb_stats$Team <- gsub("\\bSt\\.\\b", "State", cfb$Team)
+
+cfb_stats = cfb_stats %>%
+  mutate(Team = recode(Team, "App State" = "Appalachian State",
+                       ))
 
 recruiting = read.csv("recruiting_rankings.csv")
 
-cfb_stats = merge(cfb_stats, recruiting, by = "Team")
+cfb_stats_2 = merge(cfb_stats, recruiting, by = "Team")
+
+
 
 
 
